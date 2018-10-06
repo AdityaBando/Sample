9cbf4f587d8fd4a7c6d7c04e43a83dfafb8d47ec
Aditya Bando <adityabando8@gamil.com>
2018-10-06 13:32:45 +0530

    added counting in anApp

--- anApp.cpp
+++ anApp.cpp
@@ -1,6 +1,10 @@
-#inlude<iostream>
+#include<iostream>
 using namespace std;
 int main(int argc, char const *argv[]) {
   std::cout << "this is an app." << '\n';
+for (size_t i = 0; i < 10; i++) {
+  std::cout << i << ' ';
+}
+std::cout << '\n';
   return 0;
 }
